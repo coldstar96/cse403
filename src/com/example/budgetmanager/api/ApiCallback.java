212c5d629f5ec463e6de8da428c3bf22d2f9dbb6
brucec5 <aprofessionalemailaddress@gmail.com>
2013-05-01 01:50:56 -0700

    Add Javadoc comments to the API interface

--- src/com/example/budgetmanager/api/ApiCallback.java
+++ src/com/example/budgetmanager/api/ApiCallback.java
@@ -1,6 +1,24 @@
 package com.example.budgetmanager.api;
 
+/**
+ * Interface used to pass callbacks to the ApiInterface.
+ *
+ */
 public interface ApiCallback {
+
+	/**
+	 * Method to be run if the API request completed successfully.
+	 *
+	 * @param o Result from a successful API request. The API method that this
+	 * ApiCallback is passed to will define what type <code>o</code> is.
+	 */
 	public void onSuccess(Object o);
+
+	/**
+	 * Method to be run if the API request completed unsuccessfully.
+	 *
+	 * @param o Result from an unsuccessful API request. The API method that
+	 * this ApiCallback is passed to will define what type <code>o</code> is.
+	 */
 	public void onFailure(Object o);
 }
