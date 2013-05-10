761e05241589347e5c38fc0e3427cdb637734782
Andrew T. Coleman <clinger@uw.edu>
2013-05-09 18:42:15 -0700

    Add a basic test project

---
+++ UBudgetTestTest/src/com/example/budgetmanager/test/TestCaseClassTest.java
@@ -0,0 +1,24 @@
+package com.example.budgetmanager.test;
+
+import android.test.ActivityTestCase;
+
+public class TestCaseClassTest extends ActivityTestCase {
+
+	public TestCaseClassTest() {
+		
+	}
+	
+	@Override
+	protected void setUp() {
+		
+	}
+	
+	@Override
+	protected void tearDown() {
+		
+	}
+	
+	public void testTestTestTestTest() {
+		fail("This test has failed.");
+	}
+}
