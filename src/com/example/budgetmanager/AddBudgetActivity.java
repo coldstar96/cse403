075701ac6e11a3e06751e8d9e225eb0118ea9076
Ji Park <jiwpark90@gmail.com>
2013-06-01 14:25:12 -0700

    included the sign out functionality in MainActivity

--- src/com/example/budgetmanager/AddBudgetActivity.java
+++ src/com/example/budgetmanager/AddBudgetActivity.java
@@ -181,7 +181,7 @@ public class AddBudgetActivity extends Activity {
 			cancel = true;
 		}
 
-		// checks whether name is not emtpy
+		// checks whether name is not empty
 		if (mBudgetName.isEmpty()) {
 			mBudgetNameView.setError(getString(R.string.error_invalid_budget_name));
 			focusView = mBudgetNameView;
