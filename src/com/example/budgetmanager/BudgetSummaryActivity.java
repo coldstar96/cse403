632ba2b8d991d1a8dd12b4ba4a3fafeaa669245e
Andrew T. Coleman <clinger@uw.edu>
2013-06-01 14:49:08 -0700

    Mostly functional.  Still need to touch up the code, and repatch the activitys together.

--- src/com/example/budgetmanager/BudgetSummaryActivity.java
+++ src/com/example/budgetmanager/BudgetSummaryActivity.java
@@ -1,25 +1,40 @@
 package com.example.budgetmanager;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.joda.time.LocalDate;
-
-import com.example.budgetmanager.Budget.Duration;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.TextView;
 
+import com.example.budgetmanager.Budget.Duration;
+
+import org.joda.time.LocalDate;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Activity which allows users to view a summary of a single budget.
+ *
+ * @author Andrew clinger
+ */
 public class BudgetSummaryActivity extends Activity {
 
+    //Budget being viewed
 	private Budget myBudget;
+
+    //List of entries from budget that are in the current cycle.
 	private List<Entry> myEntries;
+
+    //Text views that are set programmatically.
+	private TextView budgetName;
 	private TextView budgetTotal;
+	private TextView budgetSpent;
 	private TextView budgetBalance;
 
+    /**
+	 * Sets up the budget data for display
+	 */
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -37,14 +52,15 @@ public class BudgetSummaryActivity extends Activity {
 			}
 		}
 		*/
-		        
+
+        //TODO throw exception when budget does not exist.
 		Budget budget = new Budget("budget", 50, false, new LocalDate(2000,9,9), Duration.WEEK);
 		budget.addEntry(new Entry(10, budget, "entry1", new LocalDate(2000,9,10)));
 		budget.addEntry(new Entry(5, budget, "entry1", new LocalDate(2000,9,11)));
 		budget.addEntry(new Entry(35, budget, "entry1", new LocalDate(2000,9,15)));
-		
+
 		myBudget = budget;
-		
+
         if(cycle == -1) {
         	if(myBudget.isRecurring()) {
         		cycle = myBudget.getCurrentCycle();
@@ -53,47 +69,48 @@ public class BudgetSummaryActivity extends Activity {
         	}
         }
 
-		
+
 		//Only use entries from current period.
 		//Code should be refactored to be elsewhere
 		myEntries = new ArrayList<Entry>();
 
 		for(Entry e: myBudget.getEntries()) {
-			Log.v("mytag","Will it add it?  I don't know... :/ " + e.getDate().toString());
-			Log.v("mytag", myBudget.getStartDate(cycle).toString());
-			Log.v("mytag", myBudget.getEndDate(cycle).toString());
-			Log.v("mytag", myBudget.getStartDate().toString());
-			Log.v("mytag", Boolean.toString(e.getDate().isAfter(myBudget.getStartDate(cycle))));
-			Log.v("mytag", Boolean.toString(e.getDate().isBefore(myBudget.getEndDate(cycle))));
 			if(e.getDate().isAfter(myBudget.getStartDate(cycle)) && e.getDate().isBefore(myBudget.getEndDate(cycle)) || e.getDate().isEqual(myBudget.getEndDate(cycle))) {
-				Log.v("mytag","adding entry! " + e.getNotes());
 				myEntries.add(e);
-			}	
+			}
 		}
+
         //Inflate view
 		setContentView(R.layout.activity_budget_summary);
 
+		budgetName = (TextView) findViewById(R.id.budget_name);
 		budgetTotal = (TextView) findViewById(R.id.budget_total);
+		budgetSpent = (TextView) findViewById(R.id.budget_spent);
 		budgetBalance = (TextView) findViewById(R.id.budget_balance);
-		
-		//TODO implement better way to pass entries to surface
-		((DrawBudgetGraph) findViewById(R.id.BudgetGraph)).setEntryList(myEntries, myBudget, cycle);
+
+		Collections.sort(myEntries, new EntryLogAdapter.EntryDateComparator());
+		Collections.reverse(myEntries);
+
+		((DrawBudgetGraph) findViewById(R.id.BudgetGraph)).setProperties(myEntries, myBudget, cycle);
 	}
-	
+
+	/** Called when the activity starts */
 	@Override
-	protected void onStart() {
-		super.onStart();
-		
-		//TODO currently in cents. change before merge
+	protected void onResume() {
+		super.onResume();
+
 		int totalBudget = 0;
 		int balance;
-		
-		for(Entry e: myEntries)
+
+		for(Entry e: myEntries) {
 			totalBudget += e.getAmount();
-		
+		}
+
 		balance = myBudget.getBudgetAmount() - totalBudget;
-		
-		budgetTotal.setText(Integer.toString(totalBudget));
+
+		budgetName.setText(myBudget.getName());
+		budgetTotal.setText(Integer.toString(myBudget.getBudgetAmount()));
+		budgetSpent.setText(Integer.toString(totalBudget));
 		budgetBalance.setText(Integer.toString(balance));
 	}
 }
--- src/com/example/budgetmanager/DrawBudgetGraph.java
+++ src/com/example/budgetmanager/DrawBudgetGraph.java
@@ -1,7 +1,5 @@
 package com.example.budgetmanager;
 
-import java.util.List;
-
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -9,100 +7,124 @@ import android.graphics.DashPathEffect;
 import android.graphics.Paint;
 import android.graphics.Paint.Style;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.SurfaceView;
 
+import java.util.List;
+
+/**
+ * Graph class for BudgetSummaryActivity to draw a budget
+ * to a surfaceView.
+ *
+ * @author Andrew clinger
+ */
 public class DrawBudgetGraph extends SurfaceView {
+
+    //List of entries from budget from a given cycle.
 	private List<Entry> entryList;
+
+    //Budget to draw data from.
 	private Budget budget;
+
+    //cycle of budget to display.
 	private int cycle;
-	
-	public void setEntryList(List<Entry> entryList, Budget budget, int cycle) {
+
+    //Different paints used in the graph.
+    private final Paint entryPaint;
+    private final Paint averagePaint;
+
+    /**
+	 * Returns the end date of the given cycle.
+	 *
+	 * @param cycle The cycle to calculate the end time of.
+	 * @param entryList A list of entries from budget to graph.
+	 * @param budget A Budget.
+	 * @throws IllegalArgumentException If the cycle is negative,
+	 *         or entryList or budget are null
+	 */
+	public void setProperties(List<Entry> entryList, Budget budget, int cycle) {
+        if(cycle < 0 || entryList == null || budget == null) {
+			throw new IllegalArgumentException();
+		}
 		this.entryList = entryList;
 		this.budget = budget;
 		this.cycle = cycle;
 	}
 
+	/**
+	 * Returns a DrawBudgetGraph
+	 *
+	 * @param context Context that the graph is in.
+	 * @param attrs Attributes.
+	 */
     public DrawBudgetGraph(Context context, AttributeSet attrs) {
 		super(context, attrs);
-		// TODO Auto-generated constructor stub
 		setWillNotDraw(false);
+
+		entryPaint = new Paint();
+		entryPaint.setColor(Color.GREEN);
+		entryPaint.setStrokeWidth(5);
+		entryPaint.setFlags(Paint.ANTI_ALIAS_FLAG);
+
+        averagePaint = new Paint();
+        averagePaint.setColor(Color.YELLOW);
+        averagePaint.setStyle(Style.STROKE);
+        averagePaint.setStrokeWidth(4);
+        averagePaint.setPathEffect(new DashPathEffect(new float[] {5,5}, 3));
 	}
 
-    private long priceToY(int price, int max, int height) {
+    /** Helper method to convert from a price to a Y coordinate */
+    private static long priceToY(int price, int max, int height) {
     	return height - ((price * height) / (max));
     }
-    
-    private long dateToX(long date, long start, long end, int width) {
-    	long duration = (long) (end - start);
-    	Log.v("surface", Long.toString(end - start));
-    	Log.v("surface", Long.toString(date - start));
-    	Log.v("surface", Long.toString((date - start) * (width)));
 
+    /** Helper method to convert from a date to an X coordinate */
+    private static long dateToX(long date, long start, long end, int width) {
+    	long duration = end - start;
     	return ((date - start) * (width)) / duration;
     }
-    
+
+    /**
+     * Draws the graph.
+     */
 	@Override
     protected void onDraw(Canvas canvas) {
-		Log.v("surface","Drew to the canvas");
-		int budgetMax = budget.getBudgetAmount(); //TODO get budget too
-		
-		Paint mPaint = new Paint();
-        mPaint.setColor(Color.GREEN);
-        mPaint.setStrokeWidth(5);
-        mPaint.setFlags(Paint.ANTI_ALIAS_FLAG);
+		canvas.drawColor(Color.GRAY);
+
+		int budgetMax = budget.getBudgetAmount();
 
         int height = canvas.getHeight();
         int width = canvas.getWidth();
-        
-        //Draw entries
-        //TODO sort entries by date in activity prior to this stage
-        int[] entryHeights = new int[entryList.size()];
-        
-        if(!entryList.isEmpty())
-        	entryHeights[0] = entryList.get(0).getAmount();
 
-        for(int i = 1; i < entryList.size(); i++)
-        	entryHeights[i] = entryHeights[i - 1] + entryList.get(i).getAmount();
- 
-		Log.v("surface","Created list of additive sizes. with size of " + entryList.size());
-        
+        //Draw entries
+        int[] entryHeights = new int[entryList.size()];
+
+        if(!entryList.isEmpty()) {
+			entryHeights[0] = entryList.get(0).getAmount();
+		}
+
+        for(int i = 1; i < entryList.size(); i++) {
+			entryHeights[i] = entryHeights[i - 1] + entryList.get(i).getAmount();
+		}
+
         int max = budgetMax;
-        if(entryList.size() > 0)
-        	max = Math.max(budgetMax, entryHeights[entryList.size() - 1]);
-        
+        if(entryList.size() > 0) {
+			max = Math.max(budgetMax, entryHeights[entryList.size() - 1]);
+		}
+
         long lastx = 0;
         long lasty = height;
-        
-        
+
+
         for(int i = 0; i < entryList.size(); i++) {
-        	
-        	//Temp testing method.  Need to change before code checkin.
-        	long x = dateToX(entryList.get(i).getDate().toDate().getTime(),budget.getStartDate(cycle).toDate().getTime(),budget.getEndDate(cycle).toDate().getTime(),width);
+        	long x = dateToX(entryList.get(i).getDate().toDateTimeAtStartOfDay().getMillis(),budget.getStartDate(cycle).toDateTimeAtStartOfDay().getMillis(),budget.getEndDate(cycle).toDateTimeAtStartOfDay().getMillis(),width);
         	long y = priceToY(entryHeights[i], max, height);
-        	
-			Log.v("mytag", budget.getStartDate(cycle).toString());
-			Log.v("mytag", budget.getEndDate(cycle).toString());
-			//Log.v("mytag", entryList.get(i).getCreatedAt().toDate().toString());
-			
-        	Log.v("surface", lastx + "lx, " + lasty + "ly, " + x + "x, " + y +"y");
-        	
-        	canvas.drawLine(lastx, lasty, x, y, mPaint);
+
+        	canvas.drawLine(lastx, lasty, x, y, entryPaint);
             lastx = x;
         	lasty = y;
         }
-        
-        Log.v("surface", width + " " + height);
-		Log.v("surface","Drew entry lines");
 
-        
         //Draw target line
-        mPaint.setColor(Color.YELLOW);
-        mPaint.setStyle(Style.STROKE);
-        mPaint.setStrokeWidth(4);
-        mPaint.setPathEffect(new DashPathEffect(new float[] {10,20}, 0));
-        canvas.drawLine(0, height - 1, width - 1, priceToY(budgetMax, max, height), mPaint);
-		Log.v("surface","Drew dotted line");
-        Log.v("surface","Finished drawing to the canvas");
+        canvas.drawLine(0, height - 1, width - 1, priceToY(budgetMax, max, height), averagePaint);
     }
 }
