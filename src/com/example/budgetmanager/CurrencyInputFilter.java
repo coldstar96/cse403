b09d047de369ba3699fcd44cdba43483696a5ae5
Ji Park <jiwpark90@gmail.com>
2013-06-07 14:57:48 -0700

    added the CurrencyInputFilter

---
+++ src/com/example/budgetmanager/CurrencyInputFilter.java
@@ -0,0 +1,34 @@
+package com.example.budgetmanager;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import android.text.InputFilter;
+import android.text.Spanned;
+
+/**
+ * InputFilter that filters text for currency input.
+ *
+ * @author Ji jiwpark90
+ */
+public class CurrencyInputFilter implements InputFilter {
+
+	// regex pattern to match
+	Pattern mPattern = Pattern.compile("(0|[1-9]+[0-9]*)?(\\.[0-9]{0,2})?");
+
+	@Override
+	public CharSequence filter(CharSequence source, int start, int end,
+			Spanned dest, int dstart, int dend) {
+
+		String result =	dest.subSequence(0, dstart)	+ source.toString()	+
+				dest.subSequence(dend, dest.length());
+
+		Matcher matcher = mPattern.matcher(result);
+
+		if (!matcher.matches()) {
+			return dest.subSequence(dstart, dend);
+		}
+
+		return null;
+	}
+}
